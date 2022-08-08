e359e049637e7f3416f9fb1bce758d8b391b2a54
ttdat4 <dh51702456@student.stu.edu.vn>
2022-08-08 21:23:56 +0700

    up

---
+++ src/_public/assets/js/main.js
@@ -0,0 +1,17 @@
+// const $menu = $('.c-header__mb')
+// $('.c-header__btn--open').click( function() {
+//   $menu.css("display", "block");
+//   $('.c-header__btn--open').css("display", "none");
+//   $('.c-mainvisual__leaftop').css("display", "none");
+//   $('.c-mainvisual__leafright').css("display", "none");
+// });
+// $('.c-header__btn--close').click( function() {
+//   $menu.css("display", "none");
+//   $('.c-header__btn--open').css("display", "block");
+//   $('.c-mainvisual__leaftop').css("display", "block");
+//   $('.c-mainvisual__leafright').css("display", "block");
+// });
+// $(".c-header__mb--item").click(function() {
+//   $menu.css("display", "none");
+//   $('.c-header__btn--open').css("display", "block");
+// });
